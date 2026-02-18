c9e9df5b38fff0e86211b49a18ecf29122fa0941
Ruben Bakker <ruben@uncomplex.ch>
2026-02-18 11:49:15 +0100

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -1,15 +1,25 @@
-open Unix
+open Base
 
-let () =
-  (* You can use print statements as follows for debugging, they'll be visible when running tests. *)
-  Printf.eprintf "Logs from your program will appear here!\n";
+let ( let* ) = Lwt.bind
 
-  (* Create a TCP server socket *)
-  let server_socket = socket PF_INET SOCK_STREAM 0 in
-  setsockopt server_socket SO_REUSEADDR true;
-  bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 4221));
-  listen server_socket 1;
+let handle_client (_input, output) =
+  let* () = Lwt_io.write_line output "HTTP/1.1 200 OK\r\n\r\n" in
+  Lwt_io.printlf "Sent response"
 
-  let (client_socket, _) = accept server_socket in
-  close client_socket;
-  close server_socket
+let rec accept_connections server_socket =
+  let* client_socket, _addr = Lwt_unix.accept server_socket in
+  let input = Lwt_io.of_fd ~mode:Lwt_io.input client_socket in
+  let output = Lwt_io.of_fd ~mode:Lwt_io.output client_socket in
+  Lwt.async (fun () -> handle_client (input, output));
+  accept_connections server_socket
+
+let start_server port =
+  let sockaddr = Unix.(ADDR_INET (inet_addr_of_string "127.0.0.1", 4221)) in
+  let server_socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+  Lwt_unix.setsockopt server_socket Unix.SO_REUSEADDR true;
+  let* () = Lwt_unix.bind server_socket sockaddr in
+  Lwt_unix.listen server_socket 10;
+  let* () = Lwt_io.printlf "Server started on port %d" port in
+  accept_connections server_socket
+
+let () = Lwt_main.run (start_server 4221)
