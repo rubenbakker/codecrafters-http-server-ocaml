a9ecea1743dcf580ff3fff744e85e373e0b30c6d
Ruben Bakker <ruben@uncomplex.ch>
2026-02-19 10:47:27 +0100

    codecrafters submit [skip ci]

--- src/response.ml
+++ src/response.ml
@@ -1,12 +1,32 @@
 open! Base
 
-let response_string_with_content content =
-  Stdlib.Printf.sprintf
-    "HTTP/1.1 200 OK\r\n\
-     Content-Type: text/plain\r\n\
-     Content-Length: %d\r\n\
-     \r\n\
-     %s"
-    (String.length content) content
+type status_t =
+  | OkStatus
+  | NotFoundStatus
+  | BadRequestStatus
+  | InternalServerErrorStatus
 
-let not_found () = "HTTP/1.1 404 Not Found\r\n\r\n"
+let status_code status =
+  match status with
+  | OkStatus -> 200
+  | BadRequestStatus -> 400
+  | NotFoundStatus -> 404
+  | InternalServerErrorStatus -> 500
+
+let status_code_text status =
+  match status with
+  | OkStatus -> "OK"
+  | BadRequestStatus -> "Bad Request"
+  | NotFoundStatus -> "Not Found"
+  | InternalServerErrorStatus -> "Internal Server Error"
+
+let response_string_with_content status content =
+  Stdlib.Printf.sprintf
+    "HTTP/1.1 %d %s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s"
+    (status_code status) (status_code_text status) (String.length content)
+    content
+
+let not_found () =
+  Stdlib.Printf.sprintf "HTTP/1.1 %d %s\r\n\r\n"
+    (status_code NotFoundStatus)
+    (status_code_text NotFoundStatus)
