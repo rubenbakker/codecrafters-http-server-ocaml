2b3851bbaa092f2afeeff8e63278d68be6124690
Ruben Bakker <ruben@uncomplex.ch>
2026-02-20 12:25:18 +0100

    codecrafters submit [skip ci]

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -6,7 +6,7 @@
 #
 # Learn more: https://codecrafters.io/program-interface
 
-set -x
+apk update && apk add pkgconf
 set -e # Exit on failure
 
 # dune build --build-dir /tmp/codecrafters-build-http-server-ocaml
